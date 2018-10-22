c7fab6703a708733024191713e27054c59982829
Arun Milton <miltononline@gmail.com>
2018-10-22 06:46:32 -0400

    completed-react-to-do-events

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
- import './App.css';
- import ToDo from './Components/ToDo.js';
+import './App.css';
+import ToDo from './Components/ToDo.js';
 
  class App extends Component {
    constructor(props) {
@@ -10,21 +10,44 @@ import React, { Component } from 'react';
     { description: 'Walk the cat', isCompleted: true },
     { description: 'Throw the dishes away', isCompleted: false },
     { description: 'Buy new dishes', isCompleted: false }
-  ]
-};
+    ],
+    newTodoDescription: ''
+  };
+}
+
+handleChange(e) {
+  this.setState({ newTodoDescription: e.target.value })
+}
+
+handleSubmit(e) {
+  e.preventDefault();
+  if (!this.state.newTodoDescription) { return }
+  const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
+  this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });
+}
+
+toggleComplete(index) {
+  const todos = this.state.todos.slice();
+  const todo = todos[index];
+  todo.isCompleted = todo.isCompleted ? false : true;
+  this.setState({ todos: todos });
 }
 
    render() {
      return (
-       <div className="App">
+        <div className="App">
         <ul>
         { this.state.todos.map( (todo, index) =>
-             <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+             <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
         )}
         </ul>
-       </div>
-     );
-   }
- }
+          <form onSubmit={ (e) => this.handleSubmit(e) }>
+          <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
+}
 
  export default App;
